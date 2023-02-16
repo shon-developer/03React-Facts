32762a37c3b211a7754ca2ffbd0bd5a17ab72650
shon <shonbwd@gmail.com>
2023-02-17 00:42:43 +0530

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../App.css";
+const Main = () => {
+  return (
+    <main>
+      <h1 className="main-title">Fun Facts About React</h1>
+      <ul className="main-facts">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Was well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Power thousands of enterprise apps, including mobile app</li>
+      </ul>
+    </main>
+  );
+};
+
+export default Main;
